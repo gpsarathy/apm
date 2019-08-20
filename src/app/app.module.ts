32254673b99c75e7a816e2881c1022511cb9dae4
Parthasarathy <gpsarathy123@gmail.com>
2019-08-21 00:49:16 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, NgModule, Input } from '@angular/core';
+
+@Component({
+    selector:"pm-products",
+    templateUrl:"./product-list.component.html"
+})
+
+export class ProductListComponent
+{
+pageTitle:string="Product List ! :-)";
+showStatus:boolean = false;
+
+products : any[] =[
+    {
+        name:"Note",
+        code:"A12",
+        status:"Y",
+        price:"Rs 56",
+        rating:"7"
+    },
+    {
+        name:"Pen",
+        code:"A32",
+        status:"N",
+        price:"Rs 2",
+        rating:"8"
+    }
+];
+private _addCode: string;
+
+get addCode():string
+{
+    return this._addCode;
+}
+
+set addCode(value)
+{
+    this.testFunc();
+    this._addCode=value;
+}
+testFunc()
+{
+    console.log("In SET");
+}
+toggleStatus()
+{
+ this.showStatus= !this.showStatus;
+}
+}
